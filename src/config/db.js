735fcbdfeeb59733c545673bcd7947b0a11f55cc
EmmanoelDantas <104928162+EmmanoelDantas@users.noreply.github.com>
2022-08-25 21:25:10 -0300

    Build: configurando banco de dados e typeorm

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '123456',
+    database: 'postgres',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
